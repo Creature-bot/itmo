787d0e9a2aad475447dea63ac27a50b68b3ad2ae
Ruslana <ruslanalimoncik@gmail.com>
2024-11-08 18:55:57 +0300

    Update code_dop1.py

--- infa/infa_sem1/laba4/dop1/code_dop1.py
+++ infa/infa_sem1/laba4/dop1/code_dop1.py
@@ -1,13 +1,8 @@
 import json
 import yaml
-import time
 
-start = time.perf_counter()
-
-with open('jsonfile.json') as json_file:
+with open('json_file.json') as json_file:
   data = json.load(json_file)
 
 with open('schedule_yaml_library.yaml', 'w') as yaml_file:
   yaml.dump(data, yaml_file, allow_unicode=True, sort_keys=False)
-
-print(time.perf_counter() - start)

4b35876b83379ef63619576cbac1e1eb0e063e4c
Ruslana <ruslanalimoncik@gmail.com>
2025-05-26 13:45:31 +0300

    Add files via upload

---
+++ proga/laba6/server/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java")
+    id ("application")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "org.example"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(project(":common"))
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
+}
+
+application {
+    mainClass.set("server.ServerMain")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
